5e35773206fe37b5cfa11ab1b5d1cc0c23100a3b
Sanjeev Kaushik <sanjeevkaushik90684@gmail.com>
2026-05-26 17:42:53 +0530

    path spreator

---
+++ basis/main.go
@@ -0,0 +1,60 @@
+//path sepreator 
+
+
+package main
+
+import (
+    "fmt";
+	"path"
+)
+
+
+// func main(){
+// 	var dir,file string
+
+// 	dir,file = path.Split("golang/main.go")
+
+// 	fmt.Println("dir :",dir)
+// 	fmt.Println("file :",file)
+
+// }
+
+
+// for short declartion
+
+// func main(){
+// 	dir , file := path.Split("golang/main.go")
+
+// 	fmt.Println("dir :",dir)
+//     fmt.Println("file :",file)
+// }
+
+
+
+//non nesscery value remove
+
+// func main(){
+// 	var dir,file string
+
+// 	_,file = path.Split("golang/main.go")
+
+// 	fmt.Println("dir :",dir)
+// 	fmt.Println("file :",file)
+
+// }
+
+
+
+
+
+
+
+func main(){
+	var dir,file string
+
+	dir,file = path.Split("home/abc/golang/main.go")
+
+	fmt.Println("dir :",dir)
+	fmt.Println("file :",file)
+
+}
